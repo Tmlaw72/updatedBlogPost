675a62d0786f01b2e7cdf73f730ff0d6355b32fb
Tamera Law <64174367+Tmlaw72@users.noreply.github.com>
2026-04-15 12:25:53 -0400

    Update Homepage.jsx to have log in and redirect to blog posts buttons

--- src/homepage/Homepage.jsx
+++ src/homepage/Homepage.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import Header from '../components/common/Header'
 import Footer from '../components/common/Footer'
 import BlogPost from '../components/blog/BlogPost'
+import {Link} from 'react-router';
 
 function Homepage() {
   return (
-    <div>
-    <Header />
-    <BlogPost />
-    <Footer />
+    <div className='homepage'>
+    <h1>Welcome to the Travel Blog</h1>
+    <button>Log In</button>
+    <Link to="/postlist">
+      <button>Blog Posts</button>
+    </Link>
     </div>
   )
 }

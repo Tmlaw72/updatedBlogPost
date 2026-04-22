f9d528cb815c8629c399428342522ff820ab7998
Jasmine23-27 <jazzshaw2005@gmail.com>
2026-04-22 17:40:46 -0400

    Implement AuthProvider with login and logout functions

--- src/components/authWrapper/AuthProvider.jsx
+++ src/components/authWrapper/AuthProvider.jsx
@@ -1,1 +1,28 @@
+import { useState } from "react";
+import { AuthContext } from "./AuthContext";
 
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  // login function
+  const login = (username, password) => {
+    if (username === "admin" && password === "1234") {
+      setUser(username);
+      return true;
+    }
+    return false;
+  };
+
+  // logout function
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;

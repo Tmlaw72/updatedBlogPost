e6e0b5ec2cf52ec4d406c4679ba1e0d7d72fe397
Jasmine23-27 <jazzshaw2005@gmail.com>
2026-04-22 17:39:44 -0400

    Add AuthContext with login and logout functionality
    Implement authentication context with login and logout functions.

--- src/components/authWrapper/AuthContext.js
+++ src/components/authWrapper/AuthContext.js
@@ -1,1 +1,38 @@
+import { createContext, useContext, useState } from "react";
 
+const AuthContext = createContext();
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  // login function
+  const login = (username, password) => {
+ 
+    if (username === "admin" && password === "1234") {
+      setUser(username);
+      return true;
+    }
+    return false;
+  };
+
+  // logout function
+  const logout = () => {
+    setUser(null);
+  };
+
+  const value = {
+    user,
+    login,
+    logout,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  );
+};

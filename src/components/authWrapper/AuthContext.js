8635643cfd81c216d5d999a9e535f5791ba52370
Tamera Law <64174367+Tmlaw72@users.noreply.github.com>
2026-04-23 01:11:37 -0400

    Update AuthContext.js
    Final Changes to authcontext

--- src/components/authWrapper/AuthContext.js
+++ src/components/authWrapper/AuthContext.js
@@ -2,37 +2,10 @@ import { createContext, useContext, useState } from "react";
 
 const AuthContext = createContext();
 
-export const useAuth = () => {
-  return useContext(AuthContext);
+export const useUsername = () => {
+    const {user} = useContext(AuthContext);
+    return user ? user : null;
 };
 
-export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-
-  // login function
-  const login = (username, password) => {
- 
-    if (username === "admin" && password === "1234") {
-      setUser(username);
-      return true;
-    }
-    return false;
-  };
-
-  // logout function
-  const logout = () => {
-    setUser(null);
-  };
-
-  const value = {
-    user,
-    login,
-    logout,
-  };
-
-  return (
-    <AuthContext.Provider value={value}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
+export const useAuth = () => useContext(AuthContext);
+export default AuthContext;

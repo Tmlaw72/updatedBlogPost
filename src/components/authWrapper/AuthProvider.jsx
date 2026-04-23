10cf3e54177fd4b5b5fef8f503f4aa68530780b6
Tamera Law <64174367+Tmlaw72@users.noreply.github.com>
2026-04-23 01:11:56 -0400

    Update AuthProvider.jsx
    final changes to authprovider

--- src/components/authWrapper/AuthProvider.jsx
+++ src/components/authWrapper/AuthProvider.jsx
@@ -1,28 +1,16 @@
+import React from "react";
+import AuthContext from "./AuthContext";
 import { useState } from "react";
-import { AuthContext } from "./AuthContext";
 
-const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-
-  // login function
-  const login = (username, password) => {
-    if (username === "admin" && password === "1234") {
-      setUser(username);
-      return true;
+export const AuthProvider = ({children}) => {
+    const [user, setUser] = useState('');
+    
+    const login = (username) => {
+        setUser(username);
     }
-    return false;
-  };
+    const logout = () => {
+        setUser(null);
+    }
 
-  // logout function
-  const logout = () => {
-    setUser(null);
-  };
-
-  return (
-    <AuthContext.Provider value={{ user, login, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
-
-export default AuthProvider;
+    return <AuthContext.Provider value={{user, login, logout}}>{children}</AuthContext.Provider>
+}

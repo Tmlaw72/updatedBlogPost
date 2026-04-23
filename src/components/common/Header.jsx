d912e760b9844deb328eec557ee98e12c5f789c7
Tamera Law <64174367+Tmlaw72@users.noreply.github.com>
2026-04-23 01:12:58 -0400

    Update Header.jsx
    final update to header with log in/out thing

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import {Link} from 'react-router';
 import { useContext } from 'react';
+import { useAuth, useUsername } from "../authWrapper/AuthContext";
 
 function Header(){
+    const username = useUsername();
+  
+
     return(
         <div className="headerr">
             <h1 className="bg-white text-3xl p-3">Travel Blog</h1>
             
-            <h3><Link to='/'>Log Out</Link></h3>
+            {/*<h3><Link to='/login'>Log In</Link></h3>*/}
+            {username ? <h3><Link to="/">Log Out</Link></h3> : <h3><Link to='/login'>Log In</Link></h3>}
             <nav className="headerr">
                 <ul className="headerr">
                     <li className="headerr">

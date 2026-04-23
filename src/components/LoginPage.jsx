ec7be2693b3a895d1d6282c9dc9a4ae3015f221b
Tamera Law <64174367+Tmlaw72@users.noreply.github.com>
2026-04-23 01:13:22 -0400

    Update LoginPage.jsx
    final log in page

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,59 +1,41 @@
 import { useState } from "react";
-import { useNavigate } from "react-router";
-import { useAuth } from "./AuthContext";
+import { Link } from 'react-router';
+import { useAuth } from "./authWrapper/AuthContext";
+import Header from "./common/Header";
 
-const LoginPage = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-
+function LoginPage() {
   const { login } = useAuth();
-  const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = (e) => {
     e.preventDefault();
-
-    const success = login(username, password);
-
-    if (success) {
-      navigate("/postlist");
-    } else {
-      setError("Invalid username or password");
-    }
-  };
+    login(username);
+  }
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <form
-        onSubmit={handleSubmit}
-        className="bg-white p-6 rounded shadow-md w-80"
-      >
-        <h2 className="text-2xl font-bold mb-4 text-center">Login</h2>
-
-        {error && (
-          <p className="text-red-500 text-sm mb-2">{error}</p>
-        )}
-
+    <div>
+      <Header />
+      <button><Link to='/'>Back</Link></button>
+      <form onSubmit={onSubmit}>
+        <h2>Login</h2>
         <input
           type="text"
           placeholder="Username"
-          className="w-full mb-3 p-2 border rounded"
-          value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
-
+<div></div>
         <input
           type="password"
           placeholder="Password"
-          className="w-full mb-4 p-2 border rounded"
-          value={password}
+          /*value={password}*/
           onChange={(e) => setPassword(e.target.value)}
         />
 
+<br></br>
         <button
-          type="submit"
-          className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
-        >
+          type="submit">
           Login
         </button>
       </form>
